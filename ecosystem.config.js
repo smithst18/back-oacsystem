1676809e1c64d4712457be8970af2186ebe24268
smithst <62660737+smithst18@users.noreply.github.com>
2024-09-24 12:07:33 -0400

    feat : enviroment file update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       interpreter: '/root/.nvm/versions/node/v16.17.0/bin/ts-node',
       env: {
         PORT:"3002",
-        DB_URI:"mongodb//localhost:27017/emsdm",
+        DB_URI:"mongodb://localhost:27017/emsdm",
         PRIVATE_KEY:"SgH78/?+_01As",
         PUBLIC_URL:"localhost:3002/public"
       },
