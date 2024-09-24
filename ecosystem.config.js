f08f84c2dbfd9db7e66eb671c28450503b575591
smithst <62660737+smithst18@users.noreply.github.com>
2024-09-24 12:02:20 -0400

    feat: ecosystem file

---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'sgap',
+      script: 'src/server.ts',
+      interpreter: '/root/.nvm/versions/node/v16.17.0/bin/ts-node',
+      env: {
+        PORT:"3002",
+        DB_URI:"mongodb//localhost:27017/emsdm",
+        PRIVATE_KEY:"SgH78/?+_01As",
+        PUBLIC_URL:"localhost:3002/public"
+      },
+    },
+  ],
+};
